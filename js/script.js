0da989cda796305375915b30d1075e43dd6dcb35
Mariia <milonigame1@gmail.com>
2023-01-26 15:13:58 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+window.addEventListener("DOMContentLoaded", ()=>{
+    const tabs=document.querySelectorAll('.tabheader__item'),
+    tabsContent=document.querySelectorAll('.tabcontent'),
+    tabsParent=document.querySelector('.tabheader__items');
+
+    function hideTabContent(){
+        tabsContent.forEach(item=>{
+            item.style.display='none'; 
+        });
+        tabs.forEach(item=>{
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i=0){
+        tabsContent[i].style.display='block';//добавляем контент
+        tabs[i].classList.add('tabheader__item_active');//добавляем актив
+    }
+
+    hideTabContent();
+    showTabContent(0);
+
+    tabsParent.addEventListener('click', (event)=>{
+        const target=event.target;
+        
+        if (target&&target.classList.contains('tabheader__item')){
+            tabs.forEach((item,i)=>{
+                if (target==item){
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+   
+});
